1ce3011c0b3f0f3a6092d1ed023cbf106de9eabf
izaacAbrams <izaac.abrams25@gmail.com>
2020-03-01 19:47:57 -0600

    added authorization token with restricted access

--- server.js
+++ server.js
@@ -1,32 +1,22 @@
 const express = require("express");
 const morgan = require("morgan");
 const app = express();
+const validTypes = require("./valid-types");
+
 require("dotenv").config();
+
+app.use(function validateBearerToken(req, res, next) {
+  const authToken = req.get("Authorization");
+  const apiToken = process.env.API_TOKEN;
+  if (!authToken || authToken.split(" ")[1] !== apiToken) {
+    return res.status(401).json({ error: "Unauthorized request" });
+  }
+  next();
+});
 app.use(morgan("dev"));
 
 app.get("/types", handleGetTypes);
 app.get("/pokemon", handleGetPokemon);
-
-const validTypes = [
-  `Bug`,
-  `Dark`,
-  `Dragon`,
-  `Electric`,
-  `Fairy`,
-  `Fighting`,
-  `Fire`,
-  `Flying`,
-  `Ghost`,
-  `Grass`,
-  `Ground`,
-  `Ice`,
-  `Normal`,
-  `Poison`,
-  `Psychic`,
-  `Rock`,
-  `Steel`,
-  `Water`
-];
 
 function handleGetTypes(req, res) {
   res.json(validTypes);

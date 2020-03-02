6df817dbdd293129dc370113b3564024488b0df7
izaacAbrams <izaac.abrams25@gmail.com>
2020-03-01 19:16:27 -0600

    added both endpoints and functions to handle

--- server.js
+++ server.js
@@ -1,12 +1,39 @@
 const express = require("express");
 const morgan = require("morgan");
-
 const app = express();
-
+require("dotenv").config();
 app.use(morgan("dev"));
-app.use((req, res) => {
-  res.send("Hello, world!");
-});
+
+app.get("/types", handleGetTypes);
+app.get("/pokemon", handleGetPokemon);
+
+const validTypes = [
+  `Bug`,
+  `Dark`,
+  `Dragon`,
+  `Electric`,
+  `Fairy`,
+  `Fighting`,
+  `Fire`,
+  `Flying`,
+  `Ghost`,
+  `Grass`,
+  `Ground`,
+  `Ice`,
+  `Normal`,
+  `Poison`,
+  `Psychic`,
+  `Rock`,
+  `Steel`,
+  `Water`
+];
+
+function handleGetTypes(req, res) {
+  res.json(validTypes);
+}
+function handleGetPokemon(req, res) {
+  res.send("Hello, Pokemon!");
+}
 
 const PORT = 8000;
 
